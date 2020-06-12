59ab69037001f2e38d9bdbb3dad5bb7015a38b66
Antonio VA Hilario <avahilario@gmail.com>
2020-06-12 04:44:42 +0000

    PDF files fetched via wget --mirror on or before 20200612-044441

---
+++ members/view_pl_print.php
@@ -0,0 +1,71 @@
+<html lang="en-US">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>House of Representatives</title>
+	<link rel="shortcut icon" href="../images/favicon.ico" />
+	<!-- Bootstrap Core CSS -->
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
+	<!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> -->
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<!-- Custom CSS -->
+	<link href="../css/custom.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<div class="container-fluid pageContent">
+		<div class="row">
+			<div class="col-md-8">
+				<div class="page-header">
+					<h4>HOUSE OF REPRESENTATIVES<span class="pull-right">PARTY LIST REPRESENTATIVES</span></h4>
+				</div>
+				<table class="table table-condensed table-responsive table-bordered">
+					<tr class='bg-warning'><td colspan='2'><strong>18th CONGRESS</strong></td></tr><tr class='info'><th>1-PACMAN</th></tr><tr><td>Pineda, Enrico A.</td><tr><td>Romero, Michael  L., Ph.D.</td><tr class='info'><th>A TEACHER</th></tr><tr><td>Umali, Ma. Victoria V.</td><tr class='info'><th>AAMBIS-OWA</th></tr><tr><td>Garin, Sharon S.</td><tr class='info'><th>ABANG LINGKOD</th></tr><tr><td>Paduano, Joseph Stephen &quot;Caraps&quot; S.</td><tr class='info'><th>ABONO</th></tr><tr><td>Estrella, Conrado III M.</td><tr class='info'><th>ACT-CIS</th></tr><tr><td>Taduran, Rowena Niña O.</td><tr><td>Tulfo, Jocelyn P.</td><tr><td>Yap, Eric Go</td><tr class='info'><th>ACT-TEACHERS</th></tr><tr><td>Castro, France L.</td><tr class='info'><th>AGAP</th></tr><tr><td>Geron, Rico B.</td><tr class='info'><th>AKO BICOL</th></tr><tr><td>Co, Elizaldy S.</td><tr><td>Garbin, Alfredo Jr. A.</td><tr class='info'><th>AKO BISAYA</th></tr><tr><td>Lagon, Sonny &quot;SL&quot; L.</td><tr class='info'><th>AKO PADAYON PILIPINO</th></tr><tr><td>Ebcas, Adriano A.</td><tr class='info'><th>ALONA</th></tr><tr><td>Villaraza-Suarez, Anna Marie</td><tr class='info'><th>AMIN</th></tr><tr><td>Sangcopan, Amihilda J.</td><tr class='info'><th>AN WARAY</th></tr><tr><td>Noel, Florencio G.</td><tr class='info'><th>ANAKALUSUGAN</th></tr><tr><td>Defensor, Michael T.</td><tr class='info'><th>ANG PROBINSYANO</th></tr><tr><td>Delos Santos, Alfred C.</td><tr><td>Ong, Ronnie L.</td><tr class='info'><th>APEC</th></tr><tr><td>Dagooc, Sergio C.</td><tr class='info'><th>BAHAY</th></tr><tr><td>Aguinaldo, Naealla Bainto</td><tr class='info'><th>BAYAN MUNA</th></tr><tr><td>Cullamat, Eufemia &quot;Ka Femia&quot; C.</td><tr><td>Gaite, Ferdinand R.</td><tr><td>Zarate, Carlos Isagani T.</td><tr class='info'><th>BH</th></tr><tr><td>Herrera-Dy, Bernadette &quot;BH&quot;</td><tr class='info'><th>BHW</th></tr><tr><td>Co, Angelica Natasha</td><tr class='info'><th>BUHAY</th></tr><tr><td>Atienza, Jose Jr. L.</td><tr class='info'><th>CIBAC</th></tr><tr><td>Rivera, Domingo C.</td><tr><td>Villanueva, Eduardo &quot;Bro. Eddie&quot; C.</td><tr class='info'><th>COOP-NATCCO</th></tr><tr><td>Canama, Sabiniano S.</td><tr class='info'><th>CWS</th></tr><tr><td>Momo, Romeo Sr. S.</td><tr class='info'><th>DIWA</th></tr><tr><td>Aglipay, Michael Edgar Y.</td><tr class='info'><th>DUMPER PTDA</th></tr><tr><td>Bautista, Claudine Diana D.</td><tr class='info'><th>GABRIELA</th></tr><tr><td>Brosas, Arlene D.</td><tr class='info'><th>GP</th></tr><tr><td>Padiernos, Jose Gay G.</td><tr class='info'><th>KABATAAN</th></tr><tr><td>Elago, Sarah Jane I.</td><tr class='info'><th>KABAYAN</th></tr><tr><td>Salo, Ron P.</td><tr class='info'><th>KALINGA</th></tr><tr><td>Saulog, Irene Gay F.</td><tr class='info'><th>KUSUG TAUSUG</th></tr><tr><td>Tan, Shernee A.</td><tr class='info'><th>LPGMA</th></tr><tr><td>Albano, Rodolfo B.<span class='text-warning'> (Deceased)</span></td><tr class='info'><th>LPGMA</th></tr><tr><td>Ty, Allan U.</td><tr class='info'><th>MAGDALO</th></tr><tr><td>Cabochan, Manuel III DG.</td><tr class='info'><th>MAGSASAKA</th></tr><tr><td>Cabatbat, Argel Joseph T.</td><tr class='info'><th>MANILA TEACHERS</th></tr><tr><td>Lacson, Virgilio S.</td><tr class='info'><th>MARINO</th></tr><tr><td>Gonzalez, Sandro L.</td><tr><td>Lopez, Jose Antonio G.<span class='text-warning'> (Dropped from the Roll)</span></td><tr><td>Lusotan, Macnell M.</td><tr class='info'><th>OFW FAMILY</th></tr><tr><td>Pacquiao, Alberto &quot;Bobby&quot; D.</td><tr class='info'><th>PATROL</th></tr><tr><td>Bustos, Jorge &quot;PATROL&quot;</td><tr class='info'><th>PBA</th></tr><tr><td>Nograles, Jericho Jonas B.</td><tr class='info'><th>PHILRECA</th></tr><tr><td>De Jesus, Presley C.</td><tr class='info'><th>PROBINSYANO AKO</th></tr><tr><td>Fariñas, Rudys Caesar I G.</td><tr><td>Singson, Jose Jr. &quot;Bonito&quot; C.</td><tr class='info'><th>RAM</th></tr><tr><td>Lim, Aloy</td><tr class='info'><th>RECOBODA</th></tr><tr><td>Guya, Godofredo N.</td><tr class='info'><th>SAGIP</th></tr><tr><td>Marcoleta, Rodante D.</td><tr class='info'><th>SENIOR CITIZENS</th></tr><tr><td>Datol, Francisco Jr. G.</td><tr class='info'><th>TGP</th></tr><tr><td>Teves, Jose Jr. &quot;Bong&quot; J.</td><tr class='info'><th>TINGOG SINIRANGAN</th></tr><tr><td>Romualdez, Yedda Marie K.</td><tr class='info'><th>TUCP</th></tr><tr><td>Mendoza, Raymond Democrito C.</td>				</table>
+			</div>
+		</div>
+	</div>
+	<div class="clearfix">&nbsp;</div>
+	<!-- jQuery -->
+	<script src="../js/jquery.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="../js/bootstrap.min.js"></script>
+	<script>
+		$(document).ready(function() {
+			$('[data-toggle="tooltip"]').tooltip();
+		});
+	</script>
+	<script type="text/javascript">
+		// create the back to top button
+		$('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
+
+		var amountScrolled = 50;
+
+		$(window).scroll(function() {
+			if ($(window).scrollTop() > amountScrolled) {
+				$('a.back-to-top').fadeIn('slow');
+			} else {
+				$('a.back-to-top').fadeOut('slow');
+			}
+		});
+
+		$('a.back-to-top, a.simple-back-to-top').click(function() {
+			$('html, body').animate({
+				scrollTop: 0
+			}, 700);
+			return false;
+		});
+	</script>
+</body>
+
+</html>
