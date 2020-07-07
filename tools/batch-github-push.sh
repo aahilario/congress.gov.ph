5559131a3abc7aba59d7ee8acf7decb63dc14e1e
Antonio VA Hilario <avahilario@gmail.com>
2020-07-07 08:28:58 +0000

    Additional 17th Congress documents

--- tools/batch-github-push.sh
+++ tools/batch-github-push.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-N=40; while true; do git status | grep -A10000 'Untracked files:' | sed -r -e 's@\t@#@g' | grep '^##' | head -n$N | sed -r -e 's@^##@git add "@g' -e 's@$@"; ((N++))@g' | bash ; find . -type f -size 0c -exec git rm --cached "{}" \; ; find . -type f -size 0c -exec rm -fv "{}" \; ; git commit -a -m "Additional 17th Congress documents" && git push || break ; sleep 120; done
+N=40; while true; do git status | grep -A10000 'Untracked files:' | sed -r -e 's@\t@#@g' | grep '^##' | head -n$N | sed -r -e 's@^##@git add "@g' -e 's@$@"; ((N++))@g' | bash ; find . -type f -size 0c -exec git rm --cached "{}" \; ; find . -type f -size 0c -exec rm -fv "{}" \; ; git commit -a -m "Additional 17th Congress documents" && git push || { echo Breakbreak; }  ; sleep 120; done
